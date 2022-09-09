f6cb0aa6fbe144450d042bd1a1d0a8263de69f4c
Fred <ricardofredt@hotmail.com>
2022-09-09 20:39:45 -0300

    initial commit

---
+++ src/globalStyle.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components"
+import Colors from "./constants/colors"
+
+export default createGlobalStyle`
+  
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  html, body, #root {
+    height: 100%;
+  }
+  body {
+    font: 15px 'Comfortaa', cursive;
+    background: ${Colors.dark};
+    color: ${Colors.white};
+    -webkit-font-smoothing: antialiased !important;
+  }
+  div{
+    display:flex;
+    flex-direction:column;
+  }
+  ul {
+    list-style: none;
+  }
+`
