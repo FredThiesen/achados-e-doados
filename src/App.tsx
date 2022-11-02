08539075a64d638db98d771ea821bba0162397c2
Fred <ricardofredt@hotmail.com>
2022-11-02 13:51:16 -0300

    mudança teste

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>Heyyy teste</p>
+				<p>Mudança que ativará o pipeline!</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
